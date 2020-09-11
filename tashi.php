534ffc26ffe69145b4ece0a773395a667493cb59
kansai-gamer <takumi@tutanota.de>
2020-09-12 07:45:56 +0900

    0912

---
+++ tashi.php
@@ -0,0 +1,13 @@
+<?php
+#今日の日記
+#いつも演算子を間違えるんだよなぁ
+$nums = array(60, 90, 50, 30, 20);
+$max = $nums[0];
+
+for($i = 0; $i < count($nums); $i++){
+    if($max < $nums[$i]){
+        $max = $nums[$i];
+    }
+}
+echo $max . "\n";
+?>
