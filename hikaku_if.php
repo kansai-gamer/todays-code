66e03b678419af8b336253a381a0b503220e4413
kansai-gamer <takumi@tutanota.de>
2020-09-13 20:36:51 +0900

    0913

---
+++ hikaku_if.php
@@ -0,0 +1,28 @@
+<?php
+$nums = array(60, 50, 20, 40, 20, 100);
+$max = $nums[0];
+if($max < $nums[0]){
+    $max = $nums[0];
+}
+
+if($max < $nums[1]){
+    $max = $nums[1];
+}
+
+if($max < $nums[2]){
+    $max = $nums[2];
+}
+
+if($max < $nums[3]){
+    $max = $nums[3];
+}
+
+if($max < $nums[4]){
+    $max = $nums[4];
+}
+
+if($max < $nums[5]){
+    $max = $nums[5];
+}
+echo $max;
+?>
