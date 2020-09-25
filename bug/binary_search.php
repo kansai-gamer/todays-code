a259062efd83b184bd9460fa0110d7a1fdf9690e
kansai-gamer <takumi@tutanota.de>
2020-09-26 03:42:32 +0900

    0926

--- bug/binary_search.php
+++ bug/binary_search.php
@@ -1,6 +1,6 @@
 <?php
 $nums = [10,40,50,20,90,10,50,34,11];
-$hit = 11;
+$hit = 10;
 sort($nums);
 #ここから工夫
 $r = 0;
@@ -8,11 +8,11 @@ $l = 0;
 $cyuu = floor(count($nums) / 2);
 for($i = 0; $i < $cyuu; $i++){
     if($nums[$i] == $hit){
-        echo "Found " . $cyuu . "th";
+        echo "Found " . $cyuu . "th\n";
     }elseif($hit < $nums[$i]){
         $cyuu = $cyuu / 2;
     }elseif($hit > $nums[$i]){
-        $cyuu = $cyuu + 1;
+        $cyuu = $cyuu + 2;
     }else{
         echo "Not Found" . "\n";
         echo $nums[$i];
