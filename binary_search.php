504368bc6dc6e840a3c7752128be3969b6d37962
kansai-gamer <takumi@tutanota.de>
2020-09-26 03:37:00 +0900

    0926

--- binary_search.php
+++ binary_search.php
@@ -3,11 +3,20 @@ $nums = [10,40,50,20,90,10,50,34,11];
 $hit = 11;
 sort($nums);
 #ここから工夫
+$r = 0;
+$l = 0;
 $cyuu = floor(count($nums) / 2);
 for($i = 0; $i < $cyuu; $i++){
-    if($nums[$cyuu] == $hit){
-        echo "Found" . $cyuu;
-    }elseif()
+    if($nums[$i] == $hit){
+        echo "Found " . $cyuu . "th";
+    }elseif($hit < $nums[$i]){
+        $cyuu = $cyuu / 2;
+    }elseif($hit > $nums[$i]){
+        $cyuu = $cyuu + 1;
+    }else{
+        echo "Not Found" . "\n";
+        echo $nums[$i];
+    }
 }
 
 ?>
