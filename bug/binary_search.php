10f5ddfb2541e8e3c073722c56f3b0e781f12c77
kansai-gamer <takumi@tutanota.de>
2020-09-26 04:51:06 +0900

    0926

--- bug/binary_search.php
+++ bug/binary_search.php
@@ -1,18 +1,20 @@
 <?php
 $nums = [10,40,50,20,90,10,50,34,11];
-$hit = 10;
+$hit = trim(fgets(STDIN));
 sort($nums);
 $r = 0;
 $l = 0;
+print_r($nums);
 #↓が変動するように作る
 $cyuu = floor(count($nums) / 2);
 for($i = 0; $i < $cyuu; $i++){
     if($nums[$i] == $hit){
         echo "Found " . $cyuu . "th\n";
+        echo $i . "\n";
     }elseif($hit < $nums[$i]){
-        $cyuu = $cyuu / 2;
+        $r = $r + 1;
     }elseif($hit > $nums[$i]){
-        $cyuu = $cyuu + 2;
+        $l = $l + 2;
     }else{
         echo "Not Found" . "\n";
         echo $nums[$i];
