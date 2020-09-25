3caf07a71b57f8bbe0feed495f347abad415d3ae
kansai-gamer <takumi@tutanota.de>
2020-09-26 03:44:06 +0900

    0926

--- bug/binary_search.php
+++ bug/binary_search.php
@@ -2,9 +2,9 @@
 $nums = [10,40,50,20,90,10,50,34,11];
 $hit = 10;
 sort($nums);
-#ここから工夫
 $r = 0;
 $l = 0;
+#↓が変動するように作る
 $cyuu = floor(count($nums) / 2);
 for($i = 0; $i < $cyuu; $i++){
     if($nums[$i] == $hit){
