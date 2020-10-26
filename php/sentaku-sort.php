3db8060131a49602f9dd436d48393eba771118dd
kansai-gamer <takumi@tutanota.de>
2020-10-27 06:53:49 +0900

    1027

---
+++ php/sentaku-sort.php
@@ -0,0 +1,24 @@
+<?php
+$nums = [3, 9, 6, 1, 2];
+$count = count($nums);
+$roop = 0;
+$tmp = 0;
+
+
+while($roop < $count){
+
+    $min = $nums[$roop];
+    for($i = $roop; $i < $count; $i++) {
+        if($min > $nums[$i]){
+            $min = $nums[$i];
+            $tmp = $nums[$roop];
+            $nums[$i] = $tmp;
+            $nums[$roop] = $min;
+        }
+    }
+    $roop++;
+}
+
+var_dump($nums)
+
+?>
